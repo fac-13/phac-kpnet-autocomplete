0716b3398f4a126bf0d2812ad223b3f2a701d14c
Parissai <parissa.jamali@gmail.com>
2018-03-21 17:05:48 +0000

    fix handler issues Relates #16

--- src/handler.js
+++ src/handler.js
@@ -14,25 +14,26 @@ handlers.serverLanding = function(request, response) {
 };
 
 handlers.serverPublic = function(request, response) {
-  fs.readFile(path.join(__dirname, "..", "public", request.url), function(
+  fs.readFile(path.join(__dirname, "..", request.url), function(
     err,
     file
   ) {
     if (err) throw err;
     const extension = request.url.split(".")[1];
+    
     const extensionType = {
       html: "text/html",
       css: "text/css",
       js: "application/javascript",
       ico: "image/x-icon"
     };
-    response.writeHead(200, { "content-type": extensionType[extension] });
+    response.writeHead(200, { "Content-Type": extensionType[extension] });
     response.end(file);
   });
 };
 
 handlers.pageNotFound = function(request, response) {
-  response.writeHead(404, { "content-type": "text/html" });
-  response.writeHead("<h1>404 Page Requested Cannot be Found</h1>");
+  response.writeHead(404, { "Content-Type": "text/html" });
+  // response.write("<h1>404 Page Requested Cannot be Found</h1>");
   response.end();
 };
