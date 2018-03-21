6771ddfc200a3ddfd32e84b93d3dd1ae6408fe56
missKatiaPunter <katiapunter@gmail.com>
2018-03-21 15:54:56 +0000

    adds search function to logic Relates #12

--- src/logic.js
+++ src/logic.js
@@ -1,10 +1,16 @@
 var logicFunctions = {
-    searchJson: function(inputItem, json){
-
-    };
+  searchJson: function(inputItem, json) {
+    let keys = Object.keys(json);
+    let result = keys.reduce(function(acc, item) {
+      if (item.startsWith(inputItem)) {
+        acc[item] = json[item];
+      }
+      return acc;
+    }, {});
+    return result;
+  }
 };
 
-
-if (typeof module !== 'undefined'){
-    module.exports = logicFunctions;
+if (typeof module !== "undefined") {
+  module.exports = logicFunctions;
 }
