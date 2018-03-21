e6f84380433a3eaa786fa87a180063aa7b3e31ec
missKatiaPunter <katiapunter@gmail.com>
2018-03-21 15:07:08 +0000

    some changes to server

--- src/server.js
+++ src/server.js
@@ -2,7 +2,5 @@ var http = require('http');
 var port = process.env.PORT || 5000;
 var router = require('./router.js');
 var server = http.createServer(router);
-
-server.listen(port, function() {
-  console.log('app is listening!');
-})
+server.listen(port);
+console.log(`server is running on local host ${port}`)
