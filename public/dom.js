872014c7596dcb58b5a7443483e06af49ab61ae1
Parissai <parissa.jamali@gmail.com>
2018-03-22 15:23:28 +0000

    fix multiple search result

--- public/dom.js
+++ public/dom.js
@@ -1,79 +1,74 @@
-const input = document.getElementById('search-box');
-const searchHeader = document.getElementById('search-header');
-const dataList = document.getElementById('dogbreeds-json');
-const submitButton = document.getElementById('submit-button');
-const sectionResults = document.getElementById('section-results');
+const input = document.getElementById("search-box");
+const searchHeader = document.getElementById("search-header");
+const dataList = document.getElementById("dogbreeds-json");
+const submitButton = document.getElementById("submit-button");
+const sectionResults = document.getElementById("section-results");
 
-
-const xhr = function (url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', function() {
-        if (xhr.status == 200) {
-            const response = JSON.parse(xhr.responseText);
-            return callback(null, response);
-        }else{
-            callback('Server error'+xhr.status);
-        }
-
-    })
-    xhr.addEventListener('error', function(){
-        callback('Server did not respond');
-    })
-    xhr.open("GET", url);
-    xhr.send();
-}
-
-const search = function(){
-    const inputTerms = input.value.toLowerCase();
-    // const resultObj = logicFunctions.searchJson(inputTerms, testJson);
-    // console.log(resultObj);
-    searchHeader.innerHTML = inputTerms;
-    const url = `/api/search?q=${inputTerms}`;
-    xhr(url, function(error, response){
-        if (error){
-            console.error(error);
-        }else{
-            dataListPopulate(response);
-        }
-    })
-}
-const clearContents = function(container){
-    input.setAttribute( "autocomplete", "off" );
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
+const xhr = function(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", function() {
+    if (xhr.status == 200) {
+      const response = JSON.parse(xhr.responseText);
+      return callback(null, response);
+    } else {
+      callback("Server error" + xhr.status);
     }
-}
-const onSubmitDogChoice = function(e){
-    clearContents(dataList);
-    clearContents(sectionResults);
-    e.preventDefault();
-    let dogValue = input.value;
-    let url = `https://dog.ceo/api/breed/${dogValue}/images/random`;
-    console.log(url);
-    xhr(url, function(error, response) {
-        if (error){
-            console.error(error);
-        }
-        displayResults(response);
-    });
-}
-const displayResults = function(res){
-    console.log(res);
-    let dogPic = document.createElement("img");
-    dogPic.src = res.message;
-    sectionResults.appendChild(dogPic);
+  });
+  xhr.addEventListener("error", function() {
+    callback("Server did not respond");
+  });
+  xhr.open("GET", url);
+  xhr.send();
+};
 
-}
-const dataListPopulate = function(dogsObject){
-    const arrayOfDogs = Object.keys(dogsObject);
-    arrayOfDogs.forEach(function(dog){
-        var option = document.createElement("option");
-        option.value = dog;
-        dataList.appendChild(option);
-    });
-}
-
+const search = function() {
+  const inputTerms = input.value.toLowerCase();
+  // const resultObj = logicFunctions.searchJson(inputTerms, testJson);
+  // console.log(resultObj);
+  searchHeader.innerHTML = inputTerms;
+  const url = `/api/search?q=${inputTerms}`;
+  xhr(url, function(error, response) {
+    if (error) {
+      console.error(error);
+    } else {
+      clearContents(dataList);
+      dataListPopulate(response);
+    }
+  });
+};
+const clearContents = function(container) {
+  input.setAttribute("autocomplete", "off");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+};
+const onSubmitDogChoice = function(e) {
+  clearContents(sectionResults);
+  e.preventDefault();
+  let dogValue = input.value;
+  let url = `https://dog.ceo/api/breed/${dogValue}/images/random`;
+  console.log(url);
+  xhr(url, function(error, response) {
+    if (error) {
+      console.error(error);
+    }
+    displayResults(response);
+  });
+};
+const displayResults = function(res) {
+  console.log(res);
+  let dogPic = document.createElement("img");
+  dogPic.src = res.message;
+  sectionResults.appendChild(dogPic);
+};
+const dataListPopulate = function(dogsObject) {
+  const arrayOfDogs = Object.keys(dogsObject);
+  arrayOfDogs.forEach(function(dog) {
+    var option = document.createElement("option");
+    option.value = dog;
+    dataList.appendChild(option);
+  });
+};
 
 input.addEventListener("keyup", search, false);
 submitButton.addEventListener("click", onSubmitDogChoice);
-
