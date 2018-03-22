98f75a7fb41a6327c71d7efc98f18a2280790f40
missKatiaPunter <katiapunter@gmail.com>
2018-03-22 09:54:24 +0000

    fixes http request and handles errors

--- public/dom.js
+++ public/dom.js
@@ -1,36 +1,38 @@
-(function(document) {
-var input = document.getElementById('search-box');
-var searchHeader = document.getElementById('search-header');
+const input = document.getElementById('search-box');
+const searchHeader = document.getElementById('search-header');
 
-var search = function(){
-    var inputTerms = input.value.toLowerCase();
-    // var resultObj = logicFunctions.searchJson(inputTerms, testJson);
+const search = function(){
+    const inputTerms = input.value.toLowerCase();
+    // const resultObj = logicFunctions.searchJson(inputTerms, testJson);
     // console.log(resultObj);
     searchHeader.innerHTML = inputTerms;
-    var results = [];
-
+    const url = `/api/search?q=${inputTerms}`;
+    xhr(url, function(error, response){
+        if (error){
+            console.error(error);
+        }else{
+            console.log("response is ", response);
+        }
+    })
 }
 
-var xhr = function (url, callback) {
-    var xhr = new XMLHttpRequest();
+const xhr = function (url, callback) {
+    const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', function() {
-    if (xhr.status == 200) {
-        var response = JSON.parse(xhr.responseText);
-        return callback(response);
-    }else{
-        try {
-             callback('Your search generated no results'); // generates an exception
-            }
-        catch (e) {
-                          // statements to handle any exceptions
-                          console.log(e); // pass exception object to error handler
-                        }
-                  }
-              });
-          xhr.open("GET", url);
-          xhr.send();
-          }
+        if (xhr.status == 200) {
+            const response = JSON.parse(xhr.responseText);
+            return callback(null, response);
+        }else{
+            callback('Server error'+xhr.status);
+        }
+
+    })
+    xhr.addEventListener('error', function(){
+        callback('Server did not respond');
+    })
+    xhr.open("GET", url);
+    xhr.send();
 }
+
 input.addEventListener("keyup", search, false);
 
-})(document);
